6d9c9cb17e7d890ab2718c03d7fc9f31afbb8317
Czareg <czarecoo@gmail.com>
2021-03-23 03:58:19 +0100

    fixed issue with feign, WORKING POC

--- b/src/main/java/com/example/b/BApplication.java
+++ b/src/main/java/com/example/b/BApplication.java
@@ -1,24 +1,15 @@
 package com.example.b;
 
-import feign.Feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BApplication.class, args);
-    }
-
-    @Bean
-    public AClient aClient() {
-        return new Feign.Builder()
-                .target(AClient.class, "http://localhost:8770/info");
     }
 }
